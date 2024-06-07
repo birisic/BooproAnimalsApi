4a35015b5f3480a294e7270541c6020f787ac2d0
Martin <martin.birisic@gmail.com>
2024-06-07 20:50:34 +0200

    Cleanup

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\LinkController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/links', [LinkController::class, "index"]);
 Route::get('/links/create', [LinkController::class, "create"]);
 Route::post('/links', [LinkController::class, "store"]);
 
